f722bbe4a7331c70ccb6f4985f5a96675b6efc9a
unknown <tdanny1808@gmail.com>
2020-02-21 12:10:16 -0600

    substitutions page

--- sapinventory.py
+++ sapinventory.py
@@ -1025,10 +1025,6 @@ one special character: !@#$%*?\n''', delay=.25)
             self.substitute_foods_screen_label_3.configure(text="Made bag with substitutes", fg='blue')
             self.substitute_foods_screen_label_3.place(relx=.66, rely=.75)
 
-        self.d_outofstock = {}
-        self.d_instock = {}
-        # just in case something is removed/ force empty
-
     # ========================================================
     #                 barcode screen functions
     # =======================================================
